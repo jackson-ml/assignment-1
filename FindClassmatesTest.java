f1914f6cf7f647979d29fe74aae3b62624c897da
Jackson Meyer-Lee <jmeye@seas.upenn.edu>
2022-07-05 21:55:09 -0400

    done!

--- FindClassmatesTest.java
+++ FindClassmatesTest.java
@@ -93,4 +93,126 @@ public class FindClassmatesTest {
 		assertTrue(response.isEmpty());
 
 	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testNullStudentArg() {
+		ff = new FriendFinder(defaultClassesDataSource, defaultStudentsDataSource);
+		Set<String> response = ff.findClassmates(null);
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testNullClassesDataSource() {
+		ff = new FriendFinder(null, defaultStudentsDataSource);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void testNullStudentsDataSource() {
+		ff = new FriendFinder(defaultClassesDataSource, null);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+	}
+
+	@Test
+	public void testNullClassmateName() {
+		StudentsDataSource mockStudentsDataSource = new StudentsDataSource() {
+
+			@Override
+			public List<Student> getStudents(String className) {
+
+				Student a = new Student(null, 102);
+				Student b = new Student("B", 103);
+
+				return List.of(a,b);
+			}
+
+		};
+
+		ff = new FriendFinder(defaultClassesDataSource, mockStudentsDataSource);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+		assertNotNull(response);
+		assertEquals(1, response.size());
+		assertTrue(response.contains("B"));
+	}
+
+	@Test
+	public void testStudentDataSourceReturnsNullForGivenClass() {
+
+		ClassesDataSource newClassesDataSource = new ClassesDataSource() {
+
+			@Override
+			public List<String> getClasses(String studentName) {
+
+				return List.of("1", "2");
+
+			}
+
+		};
+
+		StudentsDataSource newStudentsDataSource = new StudentsDataSource() {
+
+			@Override
+			public List<Student> getStudents(String className) {
+
+				Student a = new Student("A", 101);
+				Student b = new Student("B", 102);
+				Student c = new Student("C", 103);
+
+				if (className.equals("1")) {
+					return null;
+				}
+				else if (className.equals("2")) {
+					return List.of(a, b, c);
+				}
+				else return null;
+			}
+
+		};
+
+		ff = new FriendFinder(newClassesDataSource, newStudentsDataSource);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+		assertNotNull(response);
+		assertEquals(2, response.size());
+		assertTrue(response.contains("B"));
+		assertTrue(response.contains("C"));
+	}
+
+	@Test
+	public void testClassmateHasNullClasses() {
+
+		ClassesDataSource newClassesDataSource = new ClassesDataSource() {
+
+			@Override
+			public List<String> getClasses(String studentName) {
+
+				if (studentName.equals("A") || studentName.equals("C")) {
+					return List.of("1", "2");
+				}
+				else {
+					return null;
+				}
+
+			}
+
+		};
+
+		StudentsDataSource newStudentsDataSource = new StudentsDataSource() {
+
+			@Override
+			public List<Student> getStudents(String className) {
+
+				Student a = new Student("A", 101);
+				Student b = new Student("B", 102);
+				Student c = new Student("C", 103);
+
+				return List.of(a, b, c);
+			}
+
+		};
+
+		ff = new FriendFinder(newClassesDataSource, newStudentsDataSource);
+		Set<String> response = ff.findClassmates(new Student("A", 101));
+		assertNotNull(response);
+		assertEquals(1, response.size());
+		assertTrue(response.contains("C"));
+	}
 }
--- FriendFinder.java
+++ FriendFinder.java
@@ -20,10 +20,17 @@ public class FriendFinder {
 	 * who is taking the same classes as that student.
 	 */
 	public Set<String> findClassmates(Student theStudent) {
-		
+
+		if (theStudent == null) {
+			throw new IllegalArgumentException("Null Student argument input");
+		}
+
 		String name = theStudent.getName();
 		
 		// find the classes that this student is taking
+		if (classesDataSource == null) {
+			throw new IllegalStateException("Null classes data source");
+		}
 		List<String> myClasses = classesDataSource.getClasses(name);
 		if (myClasses == null) {
 			return Collections.emptySet(); // return empty Set if the student isn't taking any classes
@@ -34,15 +41,29 @@ public class FriendFinder {
 		
 		for (String myClass : myClasses) {
 			// list all the students in the class
+			if (studentsDataSource == null) {
+				throw new IllegalStateException("Null students data source");
+			}
 			List<Student> students = studentsDataSource.getStudents(myClass);
-			
+			if (students == null) {
+				continue; // skip this class if there are no other students
+			}
 			for (Student otherStudent : students) {
 				
 				// find the other classes that they're taking
+				if (otherStudent.getName() == null) {
+					continue; // skip nameless students
+				}
 				List<String> theirClasses = classesDataSource.getClasses(otherStudent.getName());
+
 							
 				// see if all of the classes that they're taking are the same as the ones this student is taking
 				boolean allSame = true;
+
+				if (theirClasses == null) {
+					continue; // if another student has no classes, then they aren't classmates
+				}
+
 				for (String c : myClasses) {
 					if (theirClasses.contains(c) == false) {
 						allSame = false;
@@ -51,6 +72,7 @@ public class FriendFinder {
 				}
 				
 				// if they're taking all the same classes, then add to the set of classmates
+
 				if (allSame) {
 					if (otherStudent.getName().equals(name) == false) {
 						classmates.add(otherStudent.getName());
